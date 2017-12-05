6f37e41bd2f8a8b9405ef41112741335ec9c607c
Yaimaoz <ny8859884@gmail.com>
2017-12-05 22:47:14 +0800

    move print cnf info into a new func"printCNF"

--- src/sat.cpp
+++ src/sat.cpp
@@ -8,20 +8,33 @@ int maxVarIndex;
 vector<vector<int>> clauses;
 string filename;
 
+void printCNF();
+
 int main(int argc, char* argv[]){
+    
+    /* check input file */
     if ( argc < 2 ){
         cerr << "EXECUTION COMMAND> \"/(EXEC_FILE) (CNF_FILE)\"" << endl;
         return 1;
     } 
+
     filename = argv[1];
+
+    /* use the cnf parser api to parse the cnf file */
     parse_DIMACS_CNF(clauses,maxVarIndex,filename.c_str());
     
-    for ( auto& clause : clauses ){
-        cout << "CLAUSE: ";
-        for ( auto var : clause )
-            cout << var << " ";
-        cout << endl;
-    }
+    printCNF();
 
     return 0;
+}
+
+void printCNF(){
+    cout << "[CNF's information]: " << endl;
+    for ( size_t i = 0 ; i < clauses.size() ; ++i ){
+        cout << "[CLAUSE " << i << "]: ";
+        for ( size_t j = 0 ; j < clauses[i].size() ; ++j )
+            cout << clauses[i][j] << " ";
+        cout << endl;
+    }
+    
 }
