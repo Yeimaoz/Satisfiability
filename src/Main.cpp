1c07f2310b9589e92b826214093e78e2fe9687da
Yaimaoz <ny8859884@gmail.com>
2017-12-07 00:53:53 +0800

    remove parser module

---
+++ src/Main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include"Parser.hpp"
+#include"SatSolver.hpp"
+using namespace std;
+
+SatSolver solver;
+
+int main(int argc, char* argv[]){
+    
+    /* check input file */
+    if ( argc < 2 ){
+        cerr << "EXECUTION COMMAND> \"/(EXEC_FILE) (CNF_FILE)\"" << endl;
+        return 1;
+    } 
+    solver.LoadCNF(argv[1]);
+    solver.ShowCNF();
+
+    return 0;
+}
+
